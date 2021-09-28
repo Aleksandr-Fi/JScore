38fda7d470bbb04569427a35303bed235143216a
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2021-09-28 18:15:20 +0300

    c7

--- js/script.js
+++ js/script.js
@@ -13,21 +13,20 @@ let winWidth = window.screen.width;
 dest();
 
 function dest() {
-    if (winWidth > 768) {
+    if (winWidth >= 768) {
         swiper.destroy();
         document.querySelector('.swiper').classList.remove('swiper');
         document.querySelector('.swiper-wrapper').classList.remove('swiper-wrapper');
 
-        let slides = document.querySelectorAll('.swiper-slide');        
-        for ( let i = 0; i < slides.length; i++) {
+        let slides = document.querySelectorAll('.swiper-slide');
+        for (let i = 0; i < slides.length; i++) {
             slide = slides[i];
             slide.classList.remove('swiper-slide')
         };
-        
+
         document.querySelector('.swiper-pagination').remove('div');
     };
 };
-
 
 console.log(winWidth)
 console.log(winWidth > 768)

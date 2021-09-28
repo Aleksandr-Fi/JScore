80548b005d27f30a93ade8ae8c2f3e938ff39166
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2021-09-28 15:30:41 +0300

    c5

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 
-new Swiper('.swiper', {
+let winWidth = window.screen.width;
+console.log(winWidth)
+console.log(winWidth > 768)
+const swiper = new Swiper('.swiper', {
 
     pagination: {
         el: '.swiper-pagination',
         clicable: true,
     },
     slidesPerView: 'auto',
-    spaceBetween: 16,
+    
 });
 
+    dest()
+
+console.log((window.matchMedia('(min-width: 768px)')).matches)
+function dest() {
+    if (winWidth > 768) {
+        swiper.destroy();
+
+        document.querySelector('.swiper').classList.remove('swiper');
+        document.querySelector('.swiper-slide').classList.remove('swiper-slide');
+        document.querySelector('.swiper-wrapper').classList.remove('swiper-wrapper')
+        document.querySelector('.swiper-pagination').classList.remove('div')
+    }
+}
 

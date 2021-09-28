968c00b572014be791e5e7d75496dc468e5875c4
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2021-09-28 23:32:00 +0300

    а5

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ let btnMore = document.querySelector('.btn-more');
 
 btnMore.onclick = function() {
     btnMore.classList.toggle('btn-more--active');
-    document.querySelector(".swiper-brands__wrapper").classList.toggle('swiper-brands__wrapper--open');
     btnMore.innerHTML = (btnMore.innerHTML === 'Скрыть') ? btnMore.innerHTML = 'Показать все' : btnMore.innerHTML = 'Скрыть';
+    document.querySelector(".swiper-brands__wrapper").classList.toggle('swiper-brands__wrapper--open');
 
 }

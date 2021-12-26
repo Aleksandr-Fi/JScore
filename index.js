3a458dc1ae505205fd22c910b462521fac8b164c
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2021-12-26 09:49:36 +0300

    c3

--- index.js
+++ index.js
@@ -4,16 +4,30 @@ app.appendChild(input)
 // console.log(app);
 input.addEventListener('keyup', searchRep.bind(this))
 
+function createElem(elTag, elClass) {
+    const el = document.createElement(elTag)
+    el.classList.toggle(elClass)
+    return el
+}
+
 async function searchRep() {
     return await fetch(`https://api.github.com/search/repositories?q=${input.value}`).then(
         (res) => {
             res.json().then(res => {
-                console.log(res);
+                console.log(res.items);
+                // res.items.forEach(item =>{
+                //     const item = document.createElement('li')
+                //     item.innerHTML = `<p>${res.items[i]}</p>`
+                //     // console.log(item);
+                //     app.appendChild(item)
+                // }) 
             })
         }
     )
 }
 
-function createRep(repData) {
-    
-}
+// function createRepList(repData) {
+//     const repElement = this.createElem('li', 'rep')
+//     repElement.innerHTML = `<p>${repData.name}</p>`
+//     return repElement
+// }

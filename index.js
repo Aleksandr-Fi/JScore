8db1e92dec980c9ecfaeb6dca1cc9cb2fde929a9
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2021-12-26 10:16:52 +0300

    c4

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ async function searchRep() {
     return await fetch(`https://api.github.com/search/repositories?q=${input.value}`).then(
         (res) => {
             res.json().then(res => {
+                console.log(res);
                 console.log(res.items);
+                const fragment = document.createDocumentFragment()
+                const reps = [res.items[0], res.items[1], res.items[2],res.items[3], res.items[4]]
+                reps.forEach(rep =>{
+                        const item = document.createElement('li')
+                        item.innerHTML = `<p>${rep.name}</p>`
+                        app.appendChild(item)
+                    })
                 // res.items.forEach(item =>{
                 //     const item = document.createElement('li')
                 //     item.innerHTML = `<p>${res.items[i]}</p>`
